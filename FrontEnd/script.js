22beb897236eaf85ea96c3565afcf735a3441270
EmyNeauport <emilie.neauport@outlook.fr>
2024-12-15 10:15:46 +0100

    step 1.1 done

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,21 @@
-async function appelerListeTravaux () {
-    const reponse = await fetch("http://localhost:5678/api/works")
-    const body = await reponse.json()
-    return body
-}
+//récupérer la liste des projets via l'API
+const reponse = await fetch("http://localhost:5678/api/works")
+const listeTravaux = await reponse.json()
+console.log(listeTravaux)
 
-async function recupererListeTravaux () {
-    const listeTravaux = await appelerListeTravaux()
-    console.log(listeTravaux)
-}
+//récupérer l'élement gallery du HTML dans le JavaScript
+const gallery = document.querySelector(".gallery")
 
-recupererListeTravaux()
+// boucle qui permet d'ajouter les projets retournés par l'API dans l'élement gallery 
+// à chaque projet est associé une image et un titre retourné par l'API
+for (let i = 0; i < listeTravaux.length; i++) {
+    let projet = document.createElement("figure")
+    let imageProjet = document.createElement("img")
+    let titreProjet = document.createElement("figcaption")
+    imageProjet.src = listeTravaux[i].imageUrl
+    titreProjet.innerText = listeTravaux[i].title
+    projet.appendChild(imageProjet)
+    projet.appendChild(titreProjet)
+    gallery.appendChild(projet)   
+}
+console.log(gallery)
