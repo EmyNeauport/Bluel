537c0aa8feda3b22752a0574e20cf092019449ca
EmyNeauport <emilie.neauport@outlook.fr>
2024-12-27 14:12:16 +0100

    3.1 done

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,10 +1,8 @@
 //récupérer les projets & catégories via l'API
 const reponseTravaux = await fetch("http://localhost:5678/api/works")
 const listeTravaux = await reponseTravaux.json()
-console.log(listeTravaux)
 const reponseCategories = await fetch ("http://localhost:5678/api/categories")
 const listeCategories = await reponseCategories.json()
-console.log(listeCategories)
 
 //récupérer les éléments du HTML
 const gallery = document.querySelector(".gallery")
@@ -36,43 +34,77 @@ function afficherProjets (projets) {
     }
 }
 
+//fonction qui permet de mettre en place les filtres
+function afficherBoutons () {
+    for (let i = 0; i < listeCategories.length; i++) {
+        let button = document.createElement("button")
+        button.innerHTML = listeCategories[i].name
+        buttonContainer.appendChild(button)
+    //gestion de l'affichage en cliquant sur le bouton
+        button.addEventListener("click", () => {
+            gallery.innerHTML = ""
+            const projetsFiltres = listeTravaux.filter(
+                (projet) => projet.categoryId === listeCategories[i].id)
+            afficherProjets(projetsFiltres)
+        })
+    }
+}
+
+//fonction qui permet de mettre en place la réinitialisation des filtres
+function afficherBoutonAll () {
+    let buttonAll = document.createElement("button")
+    buttonAll.innerHTML = "Tous"
+    buttonContainer.appendChild(buttonAll)
+    //gestion de l'affichage en cliquant sur le bouton 'Tous'
+    buttonAll.addEventListener("click", () => {
+        gallery.innerHTML = ""
+        afficherProjets (listeTravaux)
+    })
+}
+
 //fonction qui permet de modifier l'affichage en mode admin
 function afficherModeAdmin () {
     const divToHide = document.querySelector(".button-container") 
     //si l'utilisateur est connecté
     if (token) {
-    if (divToHide) {
-        //masquer les filtres
-        divToHide.style.display = "none"
-        //ajouter le bouton de modification
-        let divModif = `
-            <div>
-                <img src="./assets/icons/pen.svg" alt="Icone de modification">
-                <a id="btn-modif">modifier</a>
-            </div>
-        `
-        divTitle.innerHTML = divModif
+        if (divToHide) {
+            //masquer les filtres
+            divToHide.style.display = "none"
+            //ajouter le bouton de modification
+            let divModif = `
+                <div>
+                    <img src="./assets/icons/pen.svg" alt="Icone de modification">
+                    <a id="btn-modif">modifier</a>
+                </div>
+            `
+            divTitle.innerHTML = divModif
 
-        let b = document.querySelector("#portfolio h2")
-        divTitle.appendChild(b)
+            let portfolioTitle = document.querySelector("#portfolio h2")
+            divTitle.appendChild(portfolioTitle)
+
+            isModeAdmin = true
+        }
     }
-    }
+
 }
 
 //fonction qui permet d'afficher la modale
 function afficherModale() {
     const modal = document.createElement("aside")
+    modal.id = "modal"
     modal.innerHTML = `
-        <div>
+    <img id="arrow-left" src="./assets/icons/arrow-left.svg" alt="Flèche qui permet de retourner sur la première page de la modale">    
+    <img id="btn-close" src="./assets/icons/close.svg" alt="Croix qui permet de fermer la modale">
+        <div id="galerie-photo">
             <h3>Galerie photo</h3>
-            <div></div>
+            <div id="div-galery"></div>
             <hr />
-            <button>Ajouter une photo</button>
+            <button id="btn-ajout">Ajouter une photo</button>
         </div>
-        <div>
+        <div id="ajout-photo">
             <h3>Ajout photo</h3>
             <div>
-                <img src="./assets/icons/img.svg"" alt="Icone photo" ></img>
+                <img src="./assets/icons/img.svg" alt="Icone photo" ></img>
                 <button>+ Ajouter une photo</button>
                 <p>jpg, png : 4mo max</p>
             </div>
@@ -80,72 +112,105 @@ function afficherModale() {
                 <label>Titre</label>
                 <input type="text" id ="titre" name="titre"></input>
                 <label>Catégorie</label>
-                <input type="text" id ="categorie" name="categorie"></input>
+                <input type="text" id ="categorie" name="categorie" list="category-list"></input>
                 <hr />
                 <input type="" value="Valider"></btn>
             </form>
         </div>`
     document.body.appendChild(modal)
-    console.log(modal)
-}
+    displayModal = true
+    console.log("display modal :"+displayModal)
 
-//**************************************************/
+    const ajoutPhoto = document.getElementById("ajout-photo")
+    const galeriePhoto = document.getElementById("galerie-photo")
+    const boutonAjout = document.querySelector("#btn-ajout")
+    const boutonRetour = document.getElementById("arrow-left")
+    const boutonFermer = document.getElementById("btn-close")
+    const divGalery = document.getElementById("div-galery")
+    
+    ajoutPhoto.style.display = "none"
+    boutonRetour.style.display = "none"
+    
+    for (let i = 0; i < listeTravaux.length; i++) {
+        let projet = document.createElement("figure")
+        projet.id = "projet"
+        let imageProjet = document.createElement("img")
+        imageProjet.src = listeTravaux[i].imageUrl
+        let trashWork = document.createElement("img")
+        trashWork.src = "./assets/icons/trash.svg"
+        projet.appendChild(imageProjet)
+        projet.appendChild(trashWork)
+        divGalery.appendChild(projet)   
+        }
 
-//AFFICHER LES PROJETS
-//réinitialiser le token
-const token = ("Token in index.html:", window.sessionStorage.getItem("token"))
-if (token) {
-    window.sessionStorage.removeItem("token")
-}
+    boutonAjout.addEventListener("click", () => {
+        ajoutPhoto.style.display = "block"
+        galeriePhoto.style.display = "none"
+        boutonRetour.style.display = "block"
+    
+        let dataList = document.getElementById("category-list") // je ne comprends pas car il n'y a pas d'ID "category-list" associé à datalist
+        if (!dataList) {
+            dataList = document.createElement("datalist")
+            dataList.id = "category-list"
+            document.getElementById("ajout-photo").appendChild(dataList)
+        }
+    
+        let data = "";
+        for (let i = 0; i < listeCategories.length; i++) {
+            data += `<option value="${listeCategories[i].name}"></option>`
+        }
+        dataList.innerHTML = data
+    })
 
-//afficher les projets
-afficherProjets (listeTravaux)
+    boutonRetour.addEventListener("click", () => {
+        ajoutPhoto.style.display = "none"
+        galeriePhoto.style.display = "block"
+        boutonRetour.style.display = "none"
+    })
 
-//BOUTON - filtrer en affichant tous les projets
-//mise en place du bouton 'Tous'
-let buttonAll = document.createElement("button")
-buttonAll.innerHTML = "Tous"
-buttonContainer.appendChild(buttonAll)
-//gestion de l'affichage en cliquant sur le bouton 'Tous'
-buttonAll.addEventListener("click", () => {
-    gallery.innerHTML = ""
-    afficherProjets (listeTravaux)
-    console.log(listeTravaux)
-})
-
-//BOUTON - filtrer en fonction de la catégorie
-//mise en place des boutons
-for (let i = 0; i < listeCategories.length; i++) {
-    let button = document.createElement("button")
-    button.innerHTML = listeCategories[i].name
-    buttonContainer.appendChild(button)
-//gestion de l'affichage en cliquant sur le bouton
-    button.addEventListener("click", () => {
-        gallery.innerHTML = ""
-        const projetsFiltres = listeTravaux.filter(
-            (projet) => projet.categoryId === listeCategories[i].id)
-        afficherProjets(projetsFiltres)
-        console.log(projetsFiltres)
+    boutonFermer.addEventListener("click", () => {
+        modal.remove()
+        displayModal = false
+        console.log("display modal :"+displayModal)
     })
 }
 
 //**************************************************/
 
-//A LA CONNEXION, MODIFIER LA PAGE D'ACCUEIL
+//GENERER LA PAGE D'ACCUEIL
+
+//réinitialiser le token
+const token = ("Token in index.html:", window.sessionStorage.getItem("token"))
+if (token) {
+    //window.sessionStorage.removeItem("token")
+}
+
+//afficher les projets
+afficherProjets (listeTravaux)
+
+//afficher les boutons
+afficherBoutonAll ()
+afficherBoutons ()
+
+//**************************************************/
+
+//MODIFIER LA PAGE D'ACCUEIL EN MODE ADMIN
+let isModeAdmin = false
 afficherModeAdmin()
+console.log("mode admin :"+isModeAdmin)
 
 //**************************************************/
 
 //AU CLIC SUR LE BOUTON MODIFIER, AFFICHER LA MODALE
-const boutonModifier = document.querySelector("#btn-modif")
 
-    //if (boutonModifier) {
+//afficher la modale
+let displayModal = false
+if (isModeAdmin) {
+    const boutonModifier = document.querySelector("#btn-modif")
+    if (boutonModifier) {
         boutonModifier.addEventListener("click", (event) => {
-            event.preventDefault() // Empêche le comportement par défaut
-            afficherModale() // Affiche la modale
+            event.preventDefault()
+            afficherModale()
         })
-    //} else {
-    //    console.error("Le bouton 'modifier' n'a pas été trouvé !")
-    //}
-
-
+    }
+}
